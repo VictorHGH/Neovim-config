5fcfe1d28490df397a174d2fbd5188118f0df9d4
Victor Hugo González Hernández <64684570+Pikkirrin@users.noreply.github.com>
2022-10-16 23:48:09 -0500

    Add lazygit to floaterm

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -73,11 +73,11 @@ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+keymap("n", "<leader><leader>l", ":FloatermNew lazygit<CR>", opts)
 
 -- Spell
 keymap("n", "<leader><leader>o", ":set spell<CR>", term_opts)
 keymap("n", "<leader><leader>O", ":set nospell<CR>", term_opts)
-
 
 -- files --
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
