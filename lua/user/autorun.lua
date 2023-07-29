0a074b9632058ec0cf17cda1b48e5ad97759be1f
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2023-07-29 00:55:52 -0600

    Update autorun command

--- lua/user/autorun.lua
+++ lua/user/autorun.lua
@@ -2,14 +2,12 @@ local function setup_python_execution()
   vim.api.nvim_command("augroup exe_code")
   vim.api.nvim_command("autocmd!")
   vim.api.nvim_command(
-    "autocmd Filetype python nnoremap <buffer> <localleader>r :sp<CR> :term python3 %<CR> :startinsert<CR>"
+    "autocmd Filetype python nnoremap <buffer> <localleader>r :sp<CR>:term python3 %<CR>:startinsert<CR>"
   )
   vim.api.nvim_command(
-    "autocmd Filetype javascript nnoremap <buffer> <localleader>r :sp<CR> :term node %<CR> :startinsert<CR>"
+    "autocmd Filetype javascript nnoremap <buffer> <localleader>r :sp<CR>:term node %<CR>:startinsert<CR>"
   )
-  vim.api.nvim_command(
-    "autocmd Filetype sh nnoremap <buffer> <localleader>r :sp<CR> :term bash %<CR> :startinsert<CR>"
-  )
+  vim.api.nvim_command("autocmd Filetype sh nnoremap <buffer> <localleader>r :sp<CR>:term bash %<CR>:startinsert<CR>")
   vim.api.nvim_command("augroup END")
 end
 
