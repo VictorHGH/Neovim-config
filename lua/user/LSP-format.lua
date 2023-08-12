2dbcab1481ebd0a93be3d7599a8be6f73bdc3889
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2023-08-11 18:48:59 -0600

    New LSP configurations for javascript, python, C and Bash

--- lua/user/LSP-format.lua
+++ lua/user/LSP-format.lua
@@ -70,6 +70,56 @@ lspconfig.lua_ls.setup({
   },
 })
 
+-- lspconfig for python
+lspconfig.pylsp.setup({
+  cmd = { "pylsp" },
+  settings = {
+    pylsp = {
+      configurationSources = { "flake8" },
+      plugins = {
+        pycodestyle = { enabled = false },
+        flake8 = { enabled = true },
+        pyflakes = { enabled = false },
+        pylint = { enabled = false },
+        yapf = { enabled = false },
+        isort = { enabled = false },
+        pylsp_mypy = { enabled = false },
+      },
+    },
+  },
+})
+
+-- lspconfig for javascript
+lspconfig.tsserver.setup({
+  cmd = { "typescript-language-server", "--stdio" },
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  on_attach = function(client)
+    client.resolved_capabilities.document_formatting = false
+  end,
+  root_dir = function()
+    return vim.loop.cwd()
+  end,
+  settings = { documentFormatting = false },
+})
+
+-- lspconfig for c
+lspconfig.clangd.setup({
+  cmd = { "clangd", "--background-index" },
+  filetypes = { "c", "cpp", "objc", "objcpp" },
+  root_dir = function()
+    return vim.loop.cwd()
+  end,
+})
+
+-- lspconfig for bash
+lspconfig.bashls.setup({
+  cmd = { "bash-language-server", "start" },
+  filetypes = { "sh", "zsh" },
+  root_dir = function()
+    return vim.loop.cwd()
+  end,
+})
+
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
