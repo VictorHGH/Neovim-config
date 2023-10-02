a9f2c1642df2f8574a709719e26527f2283a1cf0
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2023-10-02 16:08:27 -0600

    New configuration for indent-blank-lines "ilb"

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -1,9 +1,6 @@
-vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
-
 require("ibl").setup({
-  space_char_blankline = " ",
-  show_current_context = true,
-  show_current_context_start = false,
-  show_end_of_line = true,
+  scope = {
+    show_start = false,
+    show_end = false,
+  }
 })
